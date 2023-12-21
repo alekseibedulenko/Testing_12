e78c4e3881cba26c974088745f86590fbef12c84
alekseii <dorogazm@gmail.com>
2023-12-21 16:25:39 +0300

    5

--- main.py
+++ main.py
@@ -6,11 +6,11 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'proverka,trtr45481254, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'proverk545555545a,trtr45481254, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Zagryzka. tkfj1542')
+    print_hi('Zagr55555yzka. tkfj1542')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
